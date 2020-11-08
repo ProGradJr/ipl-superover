370d102df6f93a51056d0710ce81aa01de198e51
Venkat <raghul.v@gmail.com>
2020-11-08 11:38:48 +0530

    sound and alertbox updated

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const $team1Score = document.getElementById("score-team1");
 const $team1Wickets = document.getElementById("wickets-team1");
 const $team2Score = document.getElementById("score-team2");
 const $team2Wickets = document.getElementById("wickets-team2");
-const $winnerBox = document.getElementById("winner");
+
+const strikeAudio = new Audio("http://bit.ly/so-ball-hit");
+const gameOverAudio = new Audio("http://bit.ly/so-crowd-cheer");
 
 var team1Score = 0;
 var team1Wickets = 0;
@@ -17,12 +19,10 @@ var turn = 1;
 const possibleOutcomes = [0, 1, 2, 3, 4, 6, "W"];
 
 function gameOver() {
-  if (team1Score > team2Score) $winnerBox.textContent = "MI wins";
-  if (team2Score > team1Score) $winnerBox.textContent = "DC wins";
-  if (team2Score === team1Score)
-    $winnerBox.textContent = "It is another superover!";
-  console.log("foo");
-  console.log($winnerBox);
+  gameOverAudio.play();
+  if (team1Score > team2Score) alert("MI wins");
+  if (team2Score > team1Score) alert("DC wins");
+  if (team2Score === team1Score) alert("It is another superover!");
 }
 
 function updateScore() {
@@ -37,6 +37,9 @@ resetButton.onclick = () => {
 };
 
 strikeButton.onclick = () => {
+  //play audio
+  strikeAudio.play();
+
   //generate random strike value
   const randomElement =
     possibleOutcomes[Math.floor(Math.random() * possibleOutcomes.length)];
@@ -81,5 +84,4 @@ strikeButton.onclick = () => {
     if (team1BallsFaced === 6 || team1Wickets === 2) turn = 2;
   }
   updateScore();
-  console.log(team1Score, team1Wickets, team2Score, team2Wickets);
 };
